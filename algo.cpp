b991da058c5d3e3bea1aa94e8a55c783a97dc327
Samjamess <361309107@qq.com>
2018-07-07 21:08:28 +0800

    import code test
    running a test

---
+++ algo.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdbool.h>
+#include<math.h>
+int main(){
+    //int a=1,b=1;
+    double db=12.0,db1=5.0;
+    double db2=pow(db,db1);
+    double db3=sqrt(2.0);
+    printf("%f",db2);
+    printf("%f",db3);
+    //printf("%.2f\n",fabs(db));
+    //printf("%.0f %.0f\n",floor(db),ceil(db));
+    //printf("%.0f %.0f\n",floor(db1),ceil(db1));
+
+    //long long bignum=1234567890123456LL;
+    //bool flag1=0,flag2=true;
+    //const double pi=3.14;
+    //printf("%d %d %d\n",flag1,flag2,a==b);
+    //char c='c';
+    //printf("%d\n%lld\n%c\n%.2f",a,bignum,c,pi);
+    return 0;
+}
